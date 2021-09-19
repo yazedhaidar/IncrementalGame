4b965dbfb77bacf35e10f973d8f7923a4e0c84f3
yazedhaidar <88135665+yazedhaidar@users.noreply.github.com>
2021-09-20 00:46:17 +0700

    Revisi

--- Assets/Scripts/ResourceController.cs
+++ Assets/Scripts/ResourceController.cs
@@ -1,172 +1,104 @@
 ﻿using UnityEngine;
+
 using UnityEngine.UI;
 
-public class ResourceController : MonoBehaviour
+ 
 
+public class ResourceController : MonoBehaviour
 {
-    public AudioSource AudioUpgrade;
-    public AudioSource AudioFailUpgrade;
+    public Button ResourceButton;
+    public Image ResourceImage;
     public Text ResourceDescription;
     public Text ResourceUpgradeCost;
     public Text ResourceUnlockCost;
-    public Button ResourceButton;
-    public Image ResourceImage;
+
+    public AudioSource UpgradeSound;
+    public AudioSource FailUpgradeSound;
+   
 
     private ResourceConfig _config;
-
     private int _level = 1;
+
     public bool IsUnlocked { get; private set; }
 
     private void Start ()
-
     {
-
         ResourceButton.onClick.AddListener (() =>
-
         {
-
             if (IsUnlocked)
-
             {
-
                 UpgradeLevel ();
-                
-                
-
             }
-
             else
-
             {
-                
                 UnlockResource ();
-
             }
-
         });
-
-        ResourceButton.onClick.AddListener (UpgradeLevel);
-
     }
-
+ 
     public void SetConfig (ResourceConfig config)
-
     {
-
         _config = config;
-
         // ToString("0") berfungsi untuk membuang angka di belakang koma
-
-        ResourceDescription.text = $"{ _config.Name } LV. { _level }\n+{ GetOutput ().ToString ("0") }";
-
-        ResourceUnlockCost.text = $"BIAYA MEMBUKA\n{ _config.UnlockCost }";
-
-        ResourceUpgradeCost.text = $"BIAYA PENINGKATAN\n{ GetUpgradeCost () }";
+        ResourceDescription.text = $"{ _config.Name } Lv. { _level }\n+{ GetOutput ().ToString ("0") }";
+        ResourceUnlockCost.text = $"Unlock Cost\n{ _config.UnlockCost }";
+        ResourceUpgradeCost.text = $"Upgrade Cost\n{ GetUpgradeCost () }";
 
         SetUnlocked (_config.UnlockCost == 0);
-
     }
-
- 
 
     public double GetOutput ()
-
     {
-
         return _config.Output * _level;
-
     }
-
- 
 
     public double GetUpgradeCost ()
-
     {
-
         return _config.UpgradeCost * _level;
-
     }
-
- 
 
     public double GetUnlockCost ()
-
     {
-
         return _config.UnlockCost;
-
     }
 
-     public void UpgradeLevel ()
-
+    public void UpgradeLevel ()
     {
-
         double upgradeCost = GetUpgradeCost ();
-
-        if (GameManager.Instance._totalGold < upgradeCost)
-
+        if (GameManager.Instance.TotalGold < upgradeCost)
         {
-
+            FailUpgradeSound.Play();
             return;
-
         }
-
-
 
         GameManager.Instance.AddGold (-upgradeCost);
-
         _level++;
-        AudioUpgrade.Play();
+        UpgradeSound.Play();
 
- 
-
-        ResourceUpgradeCost.text = $"BIAYA PENINGKATAN\n{ GetUpgradeCost () }";
-
-        ResourceDescription.text = $"{ _config.Name } LV. { _level }\n+{ GetOutput ().ToString ("0") }";
-
+        ResourceUpgradeCost.text = $"Upgrade Cost\n{ GetUpgradeCost () }";
+        ResourceDescription.text = $"{ _config.Name } Lv. { _level }\n+{ GetOutput ().ToString ("0") }";
     }
 
-   
-
     public void UnlockResource ()
-
     {
-
         double unlockCost = GetUnlockCost ();
-
-        if (GameManager.Instance._totalGold < unlockCost)
-
+        if (GameManager.Instance.TotalGold < unlockCost)
         {
-
+            FailUpgradeSound.Play();
             return;
-
         }
 
- 
-
         SetUnlocked (true);
-
         GameManager.Instance.ShowNextResource ();
 
         AchievementController.Instance.UnlockAchievement (AchievementType.UnlockResource, _config.Name);
-
     }
-
- 
 
     public void SetUnlocked (bool unlocked)
-
     {
-
         IsUnlocked = unlocked;
-
         ResourceImage.color = IsUnlocked ? Color.white : Color.grey;
-
         ResourceUnlockCost.gameObject.SetActive (!unlocked);
-
         ResourceUpgradeCost.gameObject.SetActive (unlocked);
-
     }
-
-
 }
--- Assets/Scripts/TapArea.cs
+++ Assets/Scripts/TapArea.cs
@@ -1,19 +1,13 @@
 ﻿using UnityEngine;
-
 using UnityEngine.EventSystems;
 
 public class TapArea : MonoBehaviour, IPointerDownHandler
-
 {
-    public AudioSource gameClip;
+    public AudioSource tapsound;
 
     public void OnPointerDown (PointerEventData eventData)
-
     {
-       
         GameManager.Instance.CollectByTap (eventData.position, transform);
-        gameClip.Play();
-
+        tapsound.Play();
     }
-
 }
